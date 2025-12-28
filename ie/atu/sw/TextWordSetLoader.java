6248d9988459b416db0400fa9035ac0977bd0888
Oleksandr Shyrin <hs1985irin@gmail.com>
2025-12-28 20:26:04 +0000

    Add multi-thread uploading

--- ie/atu/sw/TextWordSetLoader.java
+++ ie/atu/sw/TextWordSetLoader.java
@@ -1,6 +1,9 @@
 package ie.atu.sw;
 
 import java.util.TreeSet;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.Set;
 
 public final class TextWordSetLoader {
@@ -8,14 +11,26 @@ public final class TextWordSetLoader {
 
 	public Set<String> load(String fileName) throws Exception {
 		var lines = FileUtility.readFileToList(fileName);
-		var result = new TreeSet<String>();
+		var result = new ConcurrentSkipListSet<String>();
 
-		for (var line : lines) {
-			var tokens = wt.tokenize(line);
+		try (ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor()) {
+			System.out.println("Start processing file ...");
+			System.out.println();
 
-			for (var token : tokens) {
-				result.add(token);
+			var counter = 0;
+
+			for (var line : lines) {
+				counter += 1;
+
+				executor.submit(() -> {
+					var tokens = wt.tokenize(line);
+					result.addAll(tokens);
+				});
+
+				ConsoleUtil.printProgress(counter, lines.size());
 			}
+
+			System.out.println();
 		}
 
 		return result;
