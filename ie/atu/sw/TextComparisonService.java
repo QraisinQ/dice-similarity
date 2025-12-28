9a08e24a692254f049f7a539d5ee8277c9a18f55
Oleksandr Shyrin <hs1985irin@gmail.com>
2025-12-28 19:22:34 +0000

    Add TextComparisonService class

---
+++ ie/atu/sw/TextComparisonService.java
@@ -0,0 +1,15 @@
+package ie.atu.sw;
+
+import java.util.Set;
+
+public class TextComparisonService {
+    private Set<String> filterInput(Set<String> inputSet, Set<String> filter) {
+        return null;
+    }
+
+    public double compare(Set<String> setA, Set<String> setB, Set<String> filter) {
+
+        return 42;
+    }
+
+}
