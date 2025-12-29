de52f0d058f8c15ec821c13206eb052b900b852b
Oleksandr Shyrin <hs1985irin@gmail.com>
2025-12-29 20:43:53 +0000

    Change and add JavaDoc for SimilarityResult.java

--- ie/atu/sw/SimilarityResult.java
+++ ie/atu/sw/SimilarityResult.java
@@ -1,7 +1,12 @@
 package ie.atu.sw;
 
 /**
- * Holds the result of a comparison.
+ * Stores the result of text comparison.
+ *
+ * @param dice         Dice similarity coefficient
+ * @param sizeA        size of the first set
+ * @param sizeB        size of the second set
+ * @param intersection number of common elements
  */
 public record SimilarityResult(double dice, int sizeA, int sizeB, int intersection) {
 }
