2a27cd9425808ebc60a6d490b807059691170b7e
Oleksandr Shyrin <hs1985irin@gmail.com>
2025-12-29 19:34:52 +0000

    Add JavaDoc for Runner.java

--- ie/atu/sw/Runner.java
+++ ie/atu/sw/Runner.java
@@ -1,6 +1,17 @@
 package ie.atu.sw;
 
+/**
+ * This class starts the application.
+ */
+
 public class Runner {
+
+	/**
+	 * Starts the program.
+	 *
+	 * @param args program arguments (not used)
+	 * @throws Exception if an error occurs
+	 */
 
 	public static void main(String[] args) throws Exception {
 		new Menu().showMainMenu();
