3cf5f4fdacf978b105b8b8ee721fe4a86a97bc44
Oleksandr Shyrin <hs1985irin@gmail.com>
2025-12-29 20:38:59 +0000

    Add JavaDoc for DiceCoefficient.java

--- ie/atu/sw/DiceCoefficient.java
+++ ie/atu/sw/DiceCoefficient.java
@@ -2,20 +2,34 @@ package ie.atu.sw;
 
 import java.util.Set;
 
+/**
+ * Calculates Dice similarrity coefficient.
+ */
 public final class DiceCoefficient {
+
+    /**
+     * Calculates similarity between two sets of words.
+     *
+     * @param a first set of words
+     * @param b second set of words
+     * @return similarrity result
+     */
     public static SimilarityResult score(Set<String> a, Set<String> b) {
-        if (a.isEmpty() || b.isEmpty())
+        if (a.isEmpty() || b.isEmpty()) {
             return new SimilarityResult(0, a.size(), b.size(), 0);
+        }
 
         int intersection = 0;
         Set<String> small = a.size() <= b.size() ? a : b;
         Set<String> large = small == a ? b : a;
 
         for (String w : small) {
-            if (large.contains(w))
+            if (large.contains(w)) {
                 intersection++;
+            }
         }
-        var result = (2.0 * intersection) / (a.size() + b.size());
+
+        double result = (2.0 * intersection) / (a.size() + b.size());
 
         return new SimilarityResult(result, a.size(), b.size(), intersection);
     }
